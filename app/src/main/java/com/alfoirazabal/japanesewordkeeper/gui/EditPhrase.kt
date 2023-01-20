19d3d4370cdc4cd11136165118515d92eba22594
alfoirazabal <alfoirazabal@live.com>
2023-01-20 17:03:07 -0300

    Can now Autotranslate with a switch

--- app/src/main/java/com/alfoirazabal/japanesewordkeeper/gui/EditPhrase.kt
+++ app/src/main/java/com/alfoirazabal/japanesewordkeeper/gui/EditPhrase.kt
@@ -2,10 +2,13 @@ package com.alfoirazabal.japanesewordkeeper.gui
 
 import android.content.Context
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SwitchCompat
 import com.alfoirazabal.japanesewordkeeper.R
 import com.alfoirazabal.japanesewordkeeper.db.Database
 import com.alfoirazabal.japanesewordkeeper.db.entities.Phrase
@@ -16,13 +19,15 @@ import java.util.*
 
 class EditPhrase : AppCompatActivity() {
 
+    private val autoTranslateLooperHandler = Handler(Looper.getMainLooper())
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_phrase)
 
         val spinnerLanguage = findViewById<Spinner>(R.id.spinner_language)
         val etxtText = findViewById<EditText>(R.id.etxt_text)
-        val btnAutoTranslate = findViewById<Button>(R.id.btn_auto_translate)
+        val switchAutoTranslate = findViewById<SwitchCompat>(R.id.switch_auto_translate)
         val etxtRomaji = findViewById<EditText>(R.id.etxt_romaji)
         val etxtTranslation = findViewById<EditText>(R.id.etxt_translation)
         val btnEdit = findViewById<Button>(R.id.btn_add)
@@ -56,6 +61,28 @@ class EditPhrase : AppCompatActivity() {
             }
         }.start()
 
+        val autoTranslateHandler = AutoTranslateHandler(activity = this)
+
+        switchAutoTranslate.setOnCheckedChangeListener { _, _ ->
+            autoTranslateHandler.isTranslationOptionAvailable()
+                .addOnFailureListener { throw Error(it) }
+                .addOnCompleteListener {
+                    if (it.result == null || it.result == false) {
+                        autoTranslateHandler.startHandleDownloadModels()
+                    }
+                }
+        }
+
+        val autoTranslate = object : kotlinx.coroutines.Runnable {
+            override fun run() {
+                if (switchAutoTranslate.isChecked) {
+                    autoTranslateHandler.handle()
+                }
+                autoTranslateLooperHandler.postDelayed(this, 750)
+            }
+        }
+        this.autoTranslateLooperHandler.postDelayed(autoTranslate, 750)
+
         btnEdit.setOnClickListener {
             btnEdit.isEnabled = false
             phrase!!.text = etxtText.text.toString()
@@ -68,15 +95,11 @@ class EditPhrase : AppCompatActivity() {
                 runOnUiThread { finish() }
             }.start()
         }
+    }
 
-        btnAutoTranslate.setOnClickListener {
-            AutoTranslateHandler(
-                activity = this,
-                spinnerLanguage = spinnerLanguage,
-                etxtText = etxtText,
-                etxtTranslation = etxtTranslation
-            ).handle()
-        }
+    override fun onStop() {
+        this.autoTranslateLooperHandler.removeCallbacksAndMessages(null)
+        super.onStop()
     }
 
 }
